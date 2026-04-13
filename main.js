31e0fbd7029519a01695da5deecf9a8e7184e5a7
MohammedAref11 <245248570+MohammedAref11@users.noreply.github.com>
2026-04-13 20:59:29 +0400

    Added Generate random pass function

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-let checkLight = localStorage.getItem("lightMode"); 
-const switchBtn = document.getElementById("light-mode-btn"); 
+const switchThemeBtn = document.getElementById("light-mode-btn"); 
+const lengthText = document.getElementById("length-num");
+const rangeInput = document.getElementById("rangeInput"); 
+const generatePassBtn = document.getElementById("generatePassBtn");
+const pass1 = document.querySelector(".password-1");
+const pass2 = document.querySelector(".password-2");
+let checkLight = localStorage.getItem("lightMode");
 
-//* Enable Light Mode
-
+//* Switch Theme
 const enableLightMode = () => { 
     document.body.classList.add('light-mode'); 
     localStorage.setItem('lightMode', 'enabled');
@@ -17,7 +21,7 @@ if (checkLight === 'enabled') {
     enableLightMode(); 
 }
 
-switchBtn.addEventListener("click", () => { 
+switchThemeBtn.addEventListener("click", () => { 
     checkLight = localStorage.getItem('lightMode'); 
     if (checkLight !== 'enabled') { 
         enableLightMode(); 
@@ -25,5 +29,22 @@ switchBtn.addEventListener("click", () => {
         disableLightMode();
     }
 })
+
+const password1 = generateRandomPass(10); 
+const password2 = generateRandomPass(10); 
+
+function generateRandomPass(length) { 
+    const chars = "qwertyuiopasdfghjklzxcvbnm!@#$%^&*<>?1234567890";
+    randomPass = ""
+
+    for (let i = 0; i < length; i++) { 
+        const randomIndex = Math.floor(Math.random() * chars.length); 
+        randomPass += chars[randomIndex]
+    } 
+
+    return randomPass
+}
+
+
 
 
