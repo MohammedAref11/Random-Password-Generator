f68677624674372b510a43320686483ec6b00bcc
MohammedAref11 <245248570+MohammedAref11@users.noreply.github.com>
2026-04-14 22:45:38 +0400

    Add functions and event listeners to generate random passwords

--- main.js
+++ main.js
@@ -1,12 +1,56 @@
 const switchThemeBtn = document.getElementById("light-mode-btn"); 
-const lengthText = document.getElementById("length-num");
+const lengthNum = document.getElementById("length-num");
 const rangeInput = document.getElementById("rangeInput"); 
 const generatePassBtn = document.getElementById("generatePassBtn");
 const pass1 = document.querySelector(".password-1");
 const pass2 = document.querySelector(".password-2");
+const copyEl1 = document.getElementById("copyEl1");
+const copyEl2 = document.getElementById("copyEl2");
 let checkLight = localStorage.getItem("lightMode");
 
+let passLength = 0;
+
+function generateRandomPass() { 
+
+    const chars = "qwertyuiopasdfghjklzxcvbnm!@#$%^&*<>?1234567890";
+    randomPass = "";
+
+    for (let i = 0; i < passLength; i++) { 
+        const randomIndex = Math.floor(Math.random() * chars.length); 
+        randomPass += chars[randomIndex]
+    } 
+
+    return randomPass;
+}
+
+const test = "hello"; 
+
+generatePassBtn.addEventListener('click', () =>  { 
+    const password1 = generateRandomPass();
+    const password2 = generateRandomPass();
+    pass1.textContent = password1; 
+    pass2.textContent = password2; 
+})
+
+lengthNum.textContent = rangeInput.value; 
+passLength = rangeInput.value;
+
+rangeInput.addEventListener('input', (e) => { 
+    passLength = e.target.value; 
+    lengthNum.textContent = passLength; 
+})
+
+copyEl1.addEventListener('click', async () => { 
+    try { 
+        await navigation.clipboard.writeText(test)
+        console.log("text copied")
+    } catch(error) { 
+        console.log(error.name, error.message)
+    }
+})
+
 //* Switch Theme
+
 const enableLightMode = () => { 
     document.body.classList.add('light-mode'); 
     localStorage.setItem('lightMode', 'enabled');
@@ -29,22 +73,3 @@ switchThemeBtn.addEventListener("click", () => {
         disableLightMode();
     }
 })
-
-const password1 = generateRandomPass(10); 
-const password2 = generateRandomPass(10); 
-
-function generateRandomPass(length) { 
-    const chars = "qwertyuiopasdfghjklzxcvbnm!@#$%^&*<>?1234567890";
-    randomPass = ""
-
-    for (let i = 0; i < length; i++) { 
-        const randomIndex = Math.floor(Math.random() * chars.length); 
-        randomPass += chars[randomIndex]
-    } 
-
-    return randomPass
-}
-
-
-
-
